e8923665a9ea323cbaa66db7c94ff181e39f860f
Sk-Azraf-Sami <azraf7026official@gmail.com>
2023-05-19 22:40:33 +0600

    part-3 add Post Model

--- FineBlog/Models/ApplicationUser.cs
+++ FineBlog/Models/ApplicationUser.cs
@@ -7,5 +7,8 @@ namespace FineBlog.Models
         public string? FirstName { get; set; }  
         public string? LastName { get; set; }
 
+        //relation
+        public List<Post>? Posts { get; set; }
+
    }
 }
