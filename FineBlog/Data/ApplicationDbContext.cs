a795aee8242486793073a21eb8b5a022eee85c57
Sk-Azraf-Sami <azraf7026official@gmail.com>
2023-05-19 23:22:30 +0600

    part-4 change in appsettings.json and Program.cs

--- FineBlog/Data/ApplicationDbContext.cs
+++ FineBlog/Data/ApplicationDbContext.cs
@@ -1,6 +1,20 @@
-﻿namespace FineBlog.Data
+﻿using FineBlog.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FineBlog.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext:IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<ApplicationUser>? ApplicationUsers { get; set; }
+        public DbSet<Post>? Posts { get; set; }
+        public DbSet<Page>? Pages { get; set; }
+        
+
     }
 }
