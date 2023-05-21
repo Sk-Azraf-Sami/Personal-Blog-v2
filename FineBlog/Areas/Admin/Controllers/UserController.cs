e78160f729b55c5d845269baf8e22acee8c486ef
Sk-Azraf-Sami <azraf7026official@gmail.com>
2023-05-21 12:59:19 +0600

    part-9 admin login page now working

--- FineBlog/Areas/Admin/Controllers/UserController.cs
+++ FineBlog/Areas/Admin/Controllers/UserController.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using AspNetCoreHero.ToastNotification.Notyf;
+using FineBlog.Models;
+using FineBlog.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FineBlog.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class UserController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly INotyfService _notification; 
+        public UserController(UserManager<ApplicationUser> userManager, 
+                             SignInManager<ApplicationUser> signInManager, 
+                             INotyfService notyfService)
+        {
+            _notification = notyfService;   
+            _userManager = userManager;
+            _signInManager = signInManager; 
+        }
         public IActionResult Index()
         {
             return View();
@@ -14,7 +31,28 @@ namespace FineBlog.Areas.Admin.Controllers
         [HttpGet("Login")] // now address: https://localhost:7189/login
         public IActionResult Login()
         {
-            return View();
+            return View(new LoginVM());
+        }
+
+        // before page address : https://localhost:7189/admin/user/login/
+        [HttpPost("Login")] // now address: https://localhost:7189/login
+        public async Task<IActionResult> Login(LoginVM vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            var existingUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == vm.Username);
+            if (existingUser == null) 
+            {
+                _notification.Error("Username does not exist");
+                return View(vm);
+            }
+            await _signInManager.PasswordSignInAsync(vm.Username, vm.Password, vm.RememberMe, true);
+            _notification.Success("Login Successful");
+            return RedirectToAction("Index", "User", new { area = "Admin" });
+
         }
     }
 }
