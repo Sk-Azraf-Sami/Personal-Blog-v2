307419e29ca1e0acac2246fa9a7100f17cdeaeb2
Sk-Azraf-Sami <azraf7026official@gmail.com>
2023-06-04 20:33:14 +0600

    part-17 create fortend to show list of posts in admin pannel

--- FineBlog/ViewModels/PostVM.cs
+++ FineBlog/ViewModels/PostVM.cs
@@ -2,5 +2,11 @@
 {
     public class PostVM
     {
+        public int Id { get; set; }  
+        public string? Title { get; set; }
+        public string? AuthorName { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? ThumbnailUrl { get; set; }
+         
     }
 }
